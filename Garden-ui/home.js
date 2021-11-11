abe60470c1f9c012f458b78580fda2ceb6bef05d
PWiederspan <71568505+PWiederspan@users.noreply.github.com>
2021-11-10 16:44:55 -0800

    Added Location Service feature

--- Garden-ui/home.js
+++ Garden-ui/home.js
@@ -3,12 +3,15 @@ import '../App.css'
 import { Link } from 'react-router-dom';
 import PlantList from '../components/plant_list';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
 
 function Home({setPlantToEdit}) {
       const [plants, setPlant] = useState([]);
       const history = useHistory();
       const [location, setLocation] = useState([]);
       const [temp, setTemp] = useState([]);
+      const [url, setURL] = useState([]);
 
 
       const onDelete = async _id => {
@@ -20,16 +23,40 @@ function Home({setPlantToEdit}) {
         }
     };
 
+
       const onEdit = plant => {
           setPlantToEdit(plant);
           history.push("/edit-plant");
       }
 
+      //creating IP state
+      const [ip, setIP] = useState('');
+
+      //creating function to load ip address from the API
+      const getData = async () => {
+        const res = await axios.get('https://geolocation-db.com/json/')
+        setIP(res.data.IPv)
+        const res2 = await axios.get('http://localhost:5002/location_service/?ip_address='+res.data.IPv4)
+          console.log("Location updated to :" + res2.data.city);
+          setCity(res2.data.city)
+          setLocation(city)
+
+      }
+
+      useEffect( () => {
+        //passing getData method to the lifecycle method
+        getData()
+
+      }, []);
+
+      //creating IP state
+      const [city, setCity] = useState('');
+
       const loadPlant = async () => {
           const response = await fetch('/plants');
           const data = await response.json();
           setPlant(data);
-      }
+      };
 
       useEffect(() => {
           loadPlant();
@@ -39,12 +66,7 @@ function Home({setPlantToEdit}) {
           <>
               <h2>List of Plants</h2>
               <h3>My Location: {location}</h3>
-              <input
-               type="text"
-               placeholder="Enter location here"
-               value={temp}
-               onChange={e => setTemp(e.target.value)}/>
-               <button onClick={e => setLocation(temp)}>Update Location</button>
+               <button onClick={e => getData()}>Update Location</button>
               <PlantList plants={plants}onDelete={onDelete}onEdit={onEdit}></PlantList>
 
           </>
